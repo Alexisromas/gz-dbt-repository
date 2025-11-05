3e60797cb5a3385982bd28daa497d9148cacc3d9
alexis.romas@fk-agency.com <alexis.romas@fk-agency.com>
2025-11-05 10:25:12 +0000

    modif operational et margin

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -1,22 +1,19 @@
-with orders_margin as (
-    select * 
-    from {{ ref('int_orders_margin') }}
-),
-
-ship as (
-    select 
-        orders_id,
-        shipping_fee,
-        logcost,
-        CAST(ship_cost AS FLOAT64) as ship_cost
-    from {{ ref('stg_raw__ship') }}
-)
-
-select
-    o.orders_id,
-    o.date_date,
-    o.margin + s.shipping_fee - s.logcost - s.ship_cost as operational_margin
-from orders_margin o
-left join ship s
-    on o.orders_id = s.orders_id
-order by o.orders_id
+ SELECT
+     o.orders_id
+     ,o.date_date,
+     ROUND(
+    CAST(o.margin AS FLOAT64) 
+    + CAST(s.shipping_fee AS FLOAT64) 
+    - (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)),2
+) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,22 +1,12 @@
-with sales as (
-    select * 
-    from {{ ref('stg_raw__sales') }}
-),
-
-product as (
-    select * 
-    from {{ ref('stg_raw__product') }}
-)
-
-select
-    s.orders_id,
-    s.products_id,
-    s.date_date,
-    s.quantity,
-    s.revenue,
-    p.purchase_price,
-    CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64) AS purchase_cost,
-    s.revenue - (CAST(s.quantity AS FLOAT64) * CAST(p.purchase_price AS FLOAT64)) AS margin
-from sales s
-left join product p
-    on s.products_id = p.products_id
+SELECT
+  products_id,
+  date_date,
+  orders_id,
+  revenue,
+  quantity,
+  CAST(p.purchase_price AS FLOAT64) AS purchase_price,
+  ROUND(s.quantity * CAST(p.purchase_price AS FLOAT64), 2) AS purchase_cost,
+  ROUND(s.revenue - s.quantity * CAST(p.purchase_price AS FLOAT64), 2) AS margin
+FROM {{ ref("stg_raw__sales") }} s
+LEFT JOIN {{ ref("stg_raw__product") }} p
+USING (products_id)
