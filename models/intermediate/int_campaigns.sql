140641fb95b19421b0b0bafa28539a07390592b0
alexis.romas@fk-agency.com <alexis.romas@fk-agency.com>
2025-11-05 16:01:05 +0000

    date_date

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,22 @@
-SELECT *
-FROM {{ ref('stg_raw__adwords') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__bing') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__criteo') }}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__facebook') }}
+select
+    date_date,
+    sum(cast(ads_cost as float64)) as total_ads_cost,
+    sum(impression) as total_impressions,
+    sum(click) as total_clicks,
+    count(distinct campaign_key) as nb_campaigns
+
+from (
+    select * from {{ ref('stg_raw__adwords') }}
+    union all
+    select * from {{ ref('stg_raw__bing') }}
+    union all
+    select * from {{ ref('stg_raw__criteo') }}
+    union all
+    select * from {{ ref('stg_raw__facebook') }}
+) campaigns
+
+group by date_date
+order by date_date desc
+
+
+     
